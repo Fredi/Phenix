440fadabe300ba7f833d51c104113fa7eb6f50a1
Fredi Machado <fredisoft@gmail.com>
2011-04-25 09:14:06 -0300

    Initial commit

---
+++ app/views/layouts/application.html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Framework</title>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
