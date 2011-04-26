9c01f1474da902440a2df068d216b52c1eaafcd3
Fredi Machado <fredisoft@gmail.com>
2011-04-26 11:16:37 -0300

    Added some basic error handling to the framework

--- lib/Phoenix/Http/Request.php
+++ lib/Phoenix/Http/Request.php
@@ -22,24 +22,47 @@ class Http_Request extends Request
 
 	public function handleRequest()
 	{
-		$route = router()->getMatched();
+		try
+		{
+			$route = router()->getMatched();
 
-		$class_file = $route->controller().'_controller';
+			$class_file = $route->controller().'_controller';
 
-		$file_path = ROOT.DS."app".DS."controllers".DS.$class_file.".php";
-		if (file_exists($file_path))
-			require_once $file_path;
-		else
-			throw new ControllerNotFoundException($file_path);
+			$file_path = ROOT.DS."app".DS."controllers".DS.$class_file.".php";
+			if (file_exists($file_path))
+				require_once $file_path;
+			else
+				throw new ControllerNotFoundException($file_path);
 
-		$class_name = camelize($class_file);
+			$class_name = camelize($class_file);
 
-		if (class_exists($class_name))
-			$controller = new $class_name();
-		else
-			throw new ControllerClassNotFoundException($class_name);
+			if (class_exists($class_name))
+				$controller = new $class_name();
+			else
+				throw new ControllerClassNotFoundException($class_name);
 
-		$controller->runAction($route->action(), $route->getParams());
+			$controller->runAction($route->action(), $route->getParams());
+		}
+		catch (RouteNotMatchedException $e)
+		{
+			Phoenix::notFound($e);
+		}
+		catch (ControllerNotFoundException $e)
+		{
+			Phoenix::notFound($e);
+		}
+		catch (ControllerClassNotFoundException $e)
+		{
+			Phoenix::notFound($e);
+		}
+		catch (ViewNotFoundException $e)
+		{
+			Phoenix::notFound($e);
+		}
+		catch (Exception $e)
+		{
+			Phoenix::error($e);
+		}
 	}
 }
 
