ef687e8d2c82ed3680fe9d67ac22de6ae065e154
Fredi Machado <fredisoft@gmail.com>
2011-04-27 10:11:17 -0300

    Added the MethodOverride rack application so the RESTful routes can work properly

--- config.ru
+++ config.ru
@@ -11,6 +11,7 @@ require_once ROOT.DS."lib".DS."Phoenix".DS."Phoenix.php";
 
 
 Rack::add("ExceptionHandler", MIDDLEWARE_PATH.DS."ExceptionHandler.php");
+Rack::add("MethodOverride", MIDDLEWARE_PATH.DS."MethodOverride.php");
 Rack::add("Phoenix", null, Phoenix::getInstance());
 
 
---
+++ lib/Phoenix/middleware/MethodOverride.php
@@ -0,0 +1,27 @@
+<?php
+class MethodOverride
+{
+	private $http_methods = array("GET", "HEAD", "PUT", "POST", "DELETE", "OPTIONS");
+
+	function __construct(&$app)
+	{
+		$this->app =& $app;
+	}
+	
+	function call(&$env)
+	{
+		if ($env['REQUEST_METHOD'] == "POST")
+		{
+			$req = new Request($env);
+			$method = isset($req['_method']) ? $req['_method'] : $env['HTTP_X_HTTP_METHOD_OVERRIDE'];
+			$method = strtoupper($method);
+			if (in_array($method, $this->http_methods))
+			{
+				$env["rack.methodoverride.original_method"] = $env["REQUEST_METHOD"];
+				$env["REQUEST_METHOD"] = $method;
+			}
+		}
+
+		return $this->app->call($env);
+	}
+}
