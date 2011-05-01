f3f33306ce112f6b2ec6ffe4a4e719d492f210cc
Fredi Machado <fredisoft@gmail.com>
2011-04-30 23:10:26 -0300

    Added a base model class

---
+++ lib/Phenix/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+class BaseModel extends Model
+{
+	protected $__fields_with_errors = array();
+
+	public function table_name()
+	{
+		return self::_get_table_name();
+	}
+
+	public function errors()
+	{
+		return count($this->__fields_with_errors) ? true : false;
+	}
+
+	public function fields_with_errors($fields = null)
+	{
+		if (is_array($fields))
+			$this->__fields_with_errors = $fields;
+		else
+			return $this->__fields_with_errors;
+	}
+}
