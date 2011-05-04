a992c58137975f6f182eab4082826c723dd8c4d2
Fredi Machado <fredisoft@gmail.com>
2011-05-04 16:25:53 -0300

    Added model validation. Soon examples in the Phenix-Extras repository

--- lib/Phenix/BaseModel.php
+++ lib/Phenix/BaseModel.php
@@ -1,6 +1,8 @@
 <?php
 class BaseModel extends Model
 {
+	protected $__filters = array();
+	protected $__validations = array();
 	protected $__fields_with_errors = array();
 
 	public function table_name()
@@ -13,11 +15,72 @@ class BaseModel extends Model
 		return count($this->__fields_with_errors) ? true : false;
 	}
 
-	public function fields_with_errors($fields = null)
+	public function fields_with_errors()
 	{
-		if (is_array($fields))
-			$this->__fields_with_errors = $fields;
-		else
-			return $this->__fields_with_errors;
+		return $this->__fields_with_errors;
+	}
+
+	public function beforeFilter() {}
+	public function beforeValidation() {}
+	public function beforeSave() {}
+
+	public function save($data = array())
+	{
+		if (!empty($data))
+		{
+			foreach ($data as $field => $value)
+				$this->$field = $value;
+		}
+
+		$this->beforeFilter();
+
+		// Filter all data
+		foreach ($this->as_array() as $field => $value)
+		{
+			if (isset($this->__filters[$field]))
+				$this->$field = FilterInput::clean($this->$field, $this->__filters[$field]);
+			else
+				$this->$field = FilterInput::clean($this->$field);
+		}
+
+		$this->beforeValidation();
+
+		if ($this->validate())
+		{
+			$this->beforeSave();
+			return parent::save();
+		}
+
+		return $this;
+	}
+
+	public function validate()
+	{
+		if (empty($this->__validations))
+			return true;
+
+		$validation = new Validation($this->table_name());
+
+		foreach ($this->__validations as $field => $types)
+		{
+			foreach ($types as $type => $options)
+			{
+				if ($validation->field_with_error($field))
+					break;
+
+				$function = "validates_{$type}_of";
+				if ($type == "confirmation")
+				{
+					$confirmation = $field."_confirmation";
+					$validation->$function($this->$field, $this->$confirmation, $field, $options);
+				}
+				else
+					$validation->$function($this->$field, $field, $options);
+			}
+		}
+
+		$this->__fields_with_errors = $validation->fields_with_errors();
+
+		return !$validation->errors();
 	}
 }
---
+++ lib/Phenix/Validation.php
@@ -0,0 +1,207 @@
+<?php 
+class Validation
+{
+	private $model;
+
+	private $fields_with_errors = array();
+
+	public function __construct($model)
+	{
+		$this->model = $model;
+	}
+
+	public function errors()
+	{
+		return (!empty($this->fields_with_errors)) ? true : false;
+	}
+
+	public function fields_with_errors()
+	{
+		return $this->fields_with_errors;
+	}
+
+	public function field_with_error($field)
+	{
+		return isset($this->fields_with_errors[$field]);
+	}
+
+	/**
+	 * Validates fields with values
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("name" => array("presence" => array()));
+	 *
+	 * If you want a custom message:
+	 * $__validations = array("name" => array("presence" => array("message" => "You must enter your name")));
+	 */
+	public function validates_presence_of($value, $fieldName, $options = array())
+	{
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' is required.';
+
+		if (empty($value))
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates the if the user set an unnaceptable value
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("username" => array("exclusion" => array("in" => array("admin", "superuser"), "message" => "Username not allowed")));
+	 */
+	public function validates_exclusion_of($value, $fieldName, $options = array())
+	{
+		if (@$options['allow_null'] && ($value == null))
+			return true;
+
+		if (!isset($options['in']))
+			$options['in'] = array();
+
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' should not be one of the following: ' . join(',',$options['in']) . '.';
+
+		if (in_array($value, $options['in']))
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates using a regular expression
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("email" => array("format" => array("with" => "/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i")));
+	 */
+	public function validates_format_of($value, $fieldName, $options = array())
+	{
+		if  (@$options['allow_null'] && ($value == null))
+			return true;
+
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' has an invalid format.';
+
+		if (!isset($options['with']))
+			$options['with'] = '//';
+
+		if (!preg_match($options['with'],$value))
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates if the value is one of the values you want
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("gender" => array("inclusion", array("in" => array("m", "f"))));
+	 */
+	public function validates_inclusion_of($value, $fieldName, $options = array())
+	{
+		if (@$options['allow_null'] && ($value == null))
+			return true;
+
+		if (!isset($options['in']))
+			$options['in'] = array();
+
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' should be one of the following: ' . join(',',$options['in']) . '.';
+
+		if (!in_array($value, $options['in']))
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates the length of a value
+	 *
+	 * Examples to use in your model:
+	 * $__validations = array("name" => array("length" => array("min" => 3, "message" => "Does your name has less then 3 letters?")));
+	 * $__validations = array("name" => array("length" => array("max" => 50, "message" => "Less than 50 characters")));
+	 * $__validations = array("username" => array("length" => array("in" => array(6, 20), "message" => "Username must have at minimum 6 characters and maximum 20")));
+	 * $__validations = array("year" => array("length" => array("is" => 4, "message" => "Year with 4 digits")));
+	 */
+	public function validates_length_of($value, $fieldName, $options = array())
+	{
+		if (@$options['allow_null'] && ($value == null))
+			return true;
+
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' has the wrong length.';
+
+		$len = strlen($value);
+
+		if (isset($options['max']) && $len > $options['max'])
+			$this->fields_with_errors[$fieldName] = $options['message'];
+		elseif (isset($options['min']) && $len < $options['min'])
+			$this->fields_with_errors[$fieldName] = $options['message'];
+		elseif (isset($options['in']) && is_array($options['in']))
+		{
+			if ($len < $options['in'][0] || $len > $options['in'][1])
+				$this->fields_with_errors[$fieldName] = $options['message'];
+		}
+		elseif (isset($options['is']) && $len != $options['is'])
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates if the value is numeric
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("price" => array("numericality" => array()));
+	 */
+	public function validates_numericality_of($fieldName, $options = array())
+	{
+		if (@$options['allow_null'] && ($value == null))
+			return true;
+
+		if (!isset($options['only_integer']))
+			$options['only_integer'] = false;
+
+		if (!isset($options['message']))
+		{
+			if ($options['only_integer'])
+				$options['message'] = humanize($fieldName) . ' should be an integer.';
+			else
+				$options['message'] = humanize($fieldName) . ' should be a number.';
+		}
+
+		if (!is_numeric($value) || ($options['only_integer'] && !is_int($value)))
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates if the value already exists in your table
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("email" => array("uniqueness" => array()));
+	 */
+	public function validates_uniqueness_of($value, $fieldName, $options = array())
+	{
+		if (@$options['allow_null'] && ($value == null))
+			return true;
+
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' is already taken.';
+
+		if (Model::factory($this->model)->where($fieldName, $value)->find_one())
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+
+	/**
+	 * Validates the value using an existing function that return true if valid or false if invalid
+	 *
+	 * Example to use in your model:
+	 * $__validations = array("text" => array("with_function", array("some_function", "message" => "Something went wrong")));
+	 */
+	public function validates_with_function_of($value, $fieldName, $options = array())
+	{
+		if (!isset($options['message']))
+			$options['message'] = humanize($fieldName) . ' is invalid.';
+
+		if (!isset($options['function']) || !is_callable($options['function']))
+		{
+			$this->fields_with_errors[$fieldName] = $options['message'];
+			return;
+		}
+
+		$function = $options['function'];
+
+		if (!$function($value))
+			$this->fields_with_errors[$fieldName] = $options['message'];
+	}
+}
