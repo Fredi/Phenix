1fe7eaedf5e729b3d528f16d3bb3358fa840c133
Fredi Machado <fredisoft@gmail.com>
2011-05-05 14:41:53 -0300

    Added the input filtering class, without it the model doesn't work at all

---
+++ lib/Phenix/FilterInput.php
@@ -0,0 +1,106 @@
+<?php
+class FilterInput
+{
+	private static $instance;
+
+	public static function getInstance()
+	{
+		if (!isset(self::$instance))
+			self::$instance = new self();
+		return self::$instance;
+	}
+
+	public static function clean($source, $type = 'string')
+	{
+		// Handle the type constraint
+		switch (strtoupper($type))
+		{
+			case 'INT' :
+			case 'INTEGER' :
+				// Only use the first integer value
+				preg_match('/-?[0-9]+/', (string) $source, $matches);
+				$result = @ (int) $matches[0];
+				break;
+
+			case 'FLOAT' :
+			case 'DOUBLE' :
+				// Only use the first floating point value
+				preg_match('/-?[0-9]+(\.[0-9]+)?/', (string) $source, $matches);
+				$result = @ (float) $matches[0];
+				break;
+
+			case 'NUM' :
+			case 'NUMBERS' :
+				$result = @ preg_replace('/[^0-9]/', '', $source);
+				break;
+
+			case 'BOOL' :
+			case 'BOOLEAN' :
+				$result = (bool) $source;
+				break;
+
+			case 'WORD' :
+				$result = (string) preg_replace('/[^A-Z_]/i', '', $source);
+				break;
+
+			case 'ALNUM' :
+				$result = (string) preg_replace('/[^A-Z0-9]/i', '', $source);
+				break;
+
+			case 'CMD' :
+				$result = (string) preg_replace('/[^A-Z0-9_\.-]/i', '', $source);
+				$result = ltrim($result, '.');
+				break;
+
+			case 'BASE64' :
+				$result = (string) preg_replace('/[^A-Z0-9\/+=]/i', '', $source);
+				break;
+
+			case 'HEX' :
+			case 'HEXADECIMAL' :
+				$result = (string) preg_replace('/[^a-f0-9]/i', '', $source);
+				break;
+
+			case 'STRING' :
+				if (isset($this) && is_a($this, 'FilterInput'))
+					$filter = $this;
+				else
+					$filter = FilterInput::getInstance();
+				$result = (string) htmlspecialchars(strip_tags($filter->_decode((string) $source)));
+				break;
+
+			case 'ARRAY' :
+				$result = (array) $source;
+				break;
+
+			case 'PATH' :
+				$pattern = '/^[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*([\\\\\/][A-Za-z0-9_-]+[A-Za-z0-9_\.-]*)*$/';
+				preg_match($pattern, (string) $source, $matches);
+				$result = @ (string) $matches[0];
+				break;
+
+			case 'USERNAME' :
+				$result = (string) preg_replace('/[\x00-\x1F\x7F<>"\'%&]/', '', $source);
+				break;
+
+			default :
+				break;
+		}
+		return $result;
+	}
+
+	function _decode($source)
+	{
+		// entity decode
+		$trans_tbl = get_html_translation_table(HTML_ENTITIES);
+		foreach($trans_tbl as $k => $v) {
+			$ttr[$v] = utf8_encode($k);
+		}
+		$source = strtr($source, $ttr);
+		// convert decimal
+		$source = preg_replace('/&#(\d+);/me', "utf8_encode(chr(\\1))", $source); // decimal notation
+		// convert hex
+		$source = preg_replace('/&#x([a-f0-9]+);/mei', "utf8_encode(chr(0x\\1))", $source); // hex notation
+		return $source;
+	}
+}
