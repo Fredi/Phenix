65e2446fd3f0d85646995c189ea2a64242b8107b
Fredi Machado <fredisoft@gmail.com>
2011-05-06 11:36:23 -0300

    Created a helper function to create and return a model instance with some data

--- lib/Phenix/BaseModel.php
+++ lib/Phenix/BaseModel.php
@@ -5,6 +5,12 @@ class BaseModel extends Model
 	protected $__validations = array();
 	protected $__fields_with_errors = array();
 
+	public static function create($data = null)
+	{
+		$class = get_called_class();
+		return parent::factory($class)->create($data);
+	}
+
 	public function table_name()
 	{
 		return self::_get_table_name(get_class($this));
@@ -85,5 +91,18 @@ class BaseModel extends Model
 		$this->__fields_with_errors = $validation->fields_with_errors();
 
 		return !$validation->errors();
+	}
+}
+
+if (!function_exists('get_called_class'))
+{
+	function get_called_class()
+	{
+		$bt = debug_backtrace();
+		$lines = file($bt[1]['file']);
+		preg_match('/([a-zA-Z0-9\_]+)::'.$bt[1]['function'].'/',
+			$lines[$bt[1]['line']-1],
+			$matches);
+		return $matches[1];
 	}
 }
