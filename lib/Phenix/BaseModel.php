fe6a059e9b2a8df327b06e8f856f50b8118a6f5e
Fredi Machado <fredisoft@gmail.com>
2011-05-09 22:06:00 -0300

    Little optimization

--- lib/Phenix/BaseModel.php
+++ lib/Phenix/BaseModel.php
@@ -111,9 +111,7 @@ class BaseModel extends Model
 		{
 			foreach ($data as $field => $value)
 			{
-				if (empty($this->__attr_accessible))
-					$this->$field = $value;
-				else if (in_array($field, $this->__attr_accessible))
+				if (empty($this->__attr_accessible) || in_array($field, $this->__attr_accessible))
 					$this->$field = $value;
 				else
 					unset($this->__validations[$field]);
